061a05303cab503caf0f3c9f9e884ef1366d523f
Lautaro Monserrat <lautaromonserrat@yahoo.com.ar>
2024-09-26 00:13:03 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'relevamientoVisual',
+  webDir: 'www'
+};
+
+export default config;
